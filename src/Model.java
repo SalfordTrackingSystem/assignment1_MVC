eae8bd4be48e507709187cd518ce41d6e8175db2
Sébastien Dos Santos <sebastdossantos@gmail.com>
2015-10-06 16:47:09 +0100

    /**** Simulation ****/ - add simulation_frame method

--- src/Model.java
+++ src/Model.java
@@ -49,6 +49,44 @@ public class Model {
         return frame;
     }
 
+    public void checkData(Byte[] data){
+        String flag = "";
+        if (data[0].equals(frame.SENSOR_FUSION.SB)){
+            if (data[1] == frame.SENSOR_LIR.ID){
+                if (nbByte == frame.SENSOR_LIR.CS);
+                    flag = frame.SENSOR_LIR.NAME;
+            }
+            else if (data[1] == frame.SENSOR_RIR.ID){
+                if (nbByte = frame.SENSOR_RIR.CS);
+                    flag = frame.SENSOR_RIR.NAME;
+            }
+            else if (data[1] == frame.SENSOR_FUSION.ID){
+                if (nbByte = frame.SENSOR_FUSION.CS);
+                    flag = frame.SENSOR_FUSION.NAME;
+            }
+            else if (data[1] == frame.SENSOR_MOTOR.ID){
+                if (nbByte = frame.SENSOR_MOTOR.CS);
+                    flag = frame.SENSOR_MOTOR.NAME;
+            }
+            else{
+                System.out.println("ID not valid");
+            }
+        }
+        else{
+            System.out.println("No data");
+        }
+        ///////
+        // If the flag have been changed, check the EB
+        // Print the CN of the sensor frame and valid the frame
+        // Same thing for all sensors
+        ///////
+        if (flag != ""){
+            if (data[data.length].toString() == frame.SENSOR_LIR.EB){
+                System.out.println(flag + " | CN : " + data[data.length-1]);
+            }
+        }
+    }
+
     public SerialPort getSerialPort(){
         return _sp;
     }
